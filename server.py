0ea2c8b15fc56ff3a8f0723f08d2bab48f371e98
Karan Narula <karan@KSNAir.local>
2020-03-04 12:20:14 -0800

    comments in server

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
+    ret, frame = cap.read() # read in the next frame from video feed
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # apply gray filter to the frame
+    cv2.imshow('frame',gray)    # show the modified frame
+
+    # Listen for 'q' keypress to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
